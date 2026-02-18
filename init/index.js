f98ef0184a4c9ce26257686c2eb717621b96cdae
muskanpatel3 <muskanpatel03032003@gmail.com>
2026-02-19 02:51:52 +0530

    Update project with recent changes

--- init/index.js
+++ init/index.js
@@ -5,7 +5,7 @@ const Listing = require("../models/listing.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/StayAwayProject"
 
 main().then(() => {
-    console.log("connected to DB");
+    console.log("connected to DB ");
 })
     .catch((err) => {
         console.log(err);
